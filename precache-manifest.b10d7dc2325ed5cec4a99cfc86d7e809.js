3c02d33a425d45bfbed9351b84ecec5b95b96ad4
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2021-01-03 20:44:58 -0300

    Updates

---
+++ precache-manifest.b10d7dc2325ed5cec4a99cfc86d7e809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246f05852bec3a7526e0888862215f58",
+    "url": "/scv-frontend-react-redux/index.html"
+  },
+  {
+    "revision": "c276be00523cdfe960ed",
+    "url": "/scv-frontend-react-redux/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "9b5605f027047035799f",
+    "url": "/scv-frontend-react-redux/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "c276be00523cdfe960ed",
+    "url": "/scv-frontend-react-redux/static/js/2.6e5a26ae.chunk.js"
+  },
+  {
+    "revision": "47b5c329fd041a283a7f0ad35838c75e",
+    "url": "/scv-frontend-react-redux/static/js/2.6e5a26ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5605f027047035799f",
+    "url": "/scv-frontend-react-redux/static/js/main.74c10aa8.chunk.js"
+  },
+  {
+    "revision": "e19b6a4c11f800392deb",
+    "url": "/scv-frontend-react-redux/static/js/runtime-main.26650c65.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/scv-frontend-react-redux/precache-manifest.b10d7dc2325ed5cec4a99cfc86d7e809.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scv-frontend-react-redux/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
