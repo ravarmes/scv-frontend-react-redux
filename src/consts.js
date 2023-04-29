9ed44b33d78e19ffbc1abb3b5c1cc367bae4e386
Rafael Vargas Mesquita <ravarmes@hotmail.com>
2023-04-28 23:26:46 -0300

    Render

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,5 @@
 export default {
     //API_URL: 'https://scv-backend-spring.herokuapp.com/',
-    API_URL: 'http://localhost:8080/'
+    //API_URL: 'http://localhost:8080/'
+    API_URL: 'https://scv-backend-node-sequelize.onrender.com/'
 }
